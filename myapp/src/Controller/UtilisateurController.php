9e75676117e10751aaf58b3151c9358cd1143af0
Cédric DE BRUYNE <cdebruyne@outlook.com>
2021-04-03 21:07:35 +0200

    WIP add user entrypoint

--- myapp/src/Controller/UtilisateurController.php
+++ myapp/src/Controller/UtilisateurController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,28 @@ class UtilisateurController extends AbstractController
     /**
      * @Route("/", name="homepage")
      */
-    public function index(): Response
+    public function list(): Response
     {
         return $this->render('utilisateur/index.html.twig', [
             'controller_name' => 'UtilisateurController',
         ]);
+    }
+
+    /**
+     * @Route("/utilisateur/{id<\d+>}", name="user_show")
+     * @param int $id
+     * @return Response
+     */
+    public function show(int $id): Response
+    {
+        dump($this);
+        return $this->render('utilisateur/show.html.twig', [
+            'utilisateur' => $id,
+        ]);
+    }
+
+    public function add(LoggerInterface $logger)
+    {
+        $logger->info();
     }
 }
